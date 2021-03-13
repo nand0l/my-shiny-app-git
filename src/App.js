925ee5320b976839ba874c19f0fce5823026b901
nand0l <36447266+nand0l@users.noreply.github.com>
2021-03-13 10:08:22 +0000

    added info to the homepage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.<BR>
           The App will then rebuild and redeployed the only thing is to send out a GIT commit
         </p>
         <a
