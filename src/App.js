a3ce1c449c46c06810aade41275cee72b5f795af
Nando Lutgerink <nando.lutgerink@gmail.com>
2022-05-25 10:50:10 +0200

    change 2022

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.</p>
           <p>
-          The App will then rebuild and redeployed the only thing is to send out a GIT commit</p>
+          The App will then rebuild and redeployed the only thing is to send out a GIT commit<BR/>
+          So lets check it out....</p>
         <a
           className="App-link"
           href="https://reactjs.org"
